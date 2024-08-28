e2834fb5838d45f7568a71e22ce1f2b938ea672b
delattre1 <daniieeldelattre@gmail.com>
2024-08-28 19:55:38 -0300

    Renamed ChatWithMakers to include View and created the Banners folder to organize all banner components.

---
+++ ShellMate/ViewModels/Banners/UpdateShellProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UpdateShellProfileViewModel.swift
+//  ShellMate
+//
+//  Created by Daniel Delattre on 28/08/24.
+//
+
+import Foundation
---
+++ ShellMate/Views/Banners/UpdateShellProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateShellProfile.swift
+//  ShellMate
+//
+//  Created by Daniel Delattre on 28/08/24.
+//
+
+import SwiftUI
+
+struct UpdateShellProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateShellProfile()
+}
